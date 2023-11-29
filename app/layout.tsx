b99af0879f40102a27ea2257076125b98900333f
front.marina.h <front.marina.h@alldigitalads.com>
2023-11-29 22:28:34 +0200

    wip

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,19 @@
+import { clsx } from 'clsx';
+import '@/app/ui/global.css';
+import { inter } from '@/app/ui/fonts';
+
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const bodyCX = `${inter.className} antialiased`;
+
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className={bodyCX}>
+      {children}
+      </body>
     </html>
   );
 }
---
+++ app/ui/fonts.ts
@@ -0,0 +1,25 @@
+import { Inter, Lusitana, Montserrat, Nunito, Oswald, Lato,  } from 'next/font/google';
+
+export const inter = Inter({ subsets: ['latin'] });
+
+export const lusitana = Lusitana({
+  subsets: ['latin'],
+  weight: ['400', '700'],
+});
+
+export const montserrat = Montserrat({
+  subsets: ['latin'],
+});
+
+export const nunito = Nunito({
+  subsets: ['latin'],
+});
+
+export const oswald = Oswald({
+  subsets: ['latin'],
+});
+
+export const lato = Lato({
+  subsets: ['latin'],
+  weight: ['400', '700'],
+});
